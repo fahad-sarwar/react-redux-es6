4fdae990f46fe2185de8058d1a021bc92f02b14d
fahad-sarwar <mohammed_fahad00@hotmail.com>
2017-01-14 22:44:29 +0000

    initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+﻿import {combineReducers} from 'redux';
+import courses from './courseReducer';
+import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
+
+const rootReducer = combineReducers({
+    courses, // ES6 short-hand property name
+    authors,
+    ajaxCallsInProgress
+});
+
+export default rootReducer;
